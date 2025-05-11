eab214dc0c2f0e50edf0ea2726fee1e1bcdfd2d8
gautam-rahul-09 <157109546+gautam-rahul-09@users.noreply.github.com>
2025-05-11 13:36:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,27 +15,26 @@ with open('label_encoder_gender.pkl', 'rb') as file:
 with open('onehot_encoder_geo.pkl', 'rb') as file:
     onehot_encoder_geo = pickle.load(file)
 
-with open ('scaler.pkl', 'rb') as file:
+with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 # Streamlit app
 st.title('Customer Churn Prediction')
 
-#User input
+# User input
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', min_value=18, max_value=100, value=30)
-balance = st.number_input('Balance', min_value=0, value=1000)
+balance = st.number_input('Balance', min_value=0.0, value=1000.0)
 credit_score = st.number_input('Credit Score', min_value=300, max_value=850, value=600)
-estimated_salary = st.number_input('Estimated Salary', min_value=0, value=50000)
+estimated_salary = st.number_input('Estimated Salary', min_value=0.0, value=50000.0)
 tenure = st.number_input('Tenure', min_value=0, value=5)
 num_products = st.number_input('Number of Products', min_value=1, max_value=4, value=2)
-has_cr_card = st.checkbox('Has Credit Card',[0,1])
-is_active_member = st.checkbox('Is Active Member',[0,1])
+has_cr_card = int(st.checkbox('Has Credit Card'))
+is_active_member = int(st.checkbox('Is Active Member'))
 
-#prepare the input data
+# Prepare the input data
 input_data = pd.DataFrame({
-    
     'Gender': [label_encoder_gender.transform([gender])[0]],
     'Age': [age],
     'Balance': [balance],
@@ -47,27 +46,25 @@ input_data = pd.DataFrame({
     'IsActiveMember': [is_active_member]
 })
 
-# one-hot encode geography
+# One-hot encode geography
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
-geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out('Geography'))
+geo_encoded_df = pd.DataFrame(
+    geo_encoded,
+    columns=onehot_encoder_geo.get_feature_names_out(onehot_encoder_geo.feature_names_in_)
+)
 
-# comnine one hot encoded coulmns with input data
+# Combine one-hot encoded columns with input data
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
-# scale the data
+# Scale the data
 input_data_scaled = scaler.transform(input_data)
 
-# make prediction
+# Make prediction
 prediction = model.predict(input_data_scaled)
-prediction_prob = model.prediction[0][0]
+prediction_prob = prediction[0][0]
 
-if prediction_prob  > 0.5:
-    st.write('The customer is likely to churn.')
+# Output result
+if prediction_prob > 0.5:
+    st.write('🔴 The customer is **likely to churn**.')
 else:
-    st.write('The customer is not likely to churn.')
-
-
-
-
-
-
+    st.write('🟢 The customer is **not likely to churn**.')
